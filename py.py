15db2b0f4e71b5bc88145d5968f8bb40ca5f5d53
MrMelsik <DSKozyrev@gmail.com>
2020-04-16 14:59:22 +0300

    hello

---
+++ py.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -
+import telebot
+
+TOKEN = '1235450874:AAHTVDjH_qHjhh8-k0Bc2zkyvBYPOmEMObA'
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, 'Привет')
+
+bot.polling()
